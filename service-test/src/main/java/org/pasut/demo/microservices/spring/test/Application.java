05d5e71c6646f6c11a50896bb55e9a0bab82429c
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-05-21 13:24:04 -0300

    cambios en service-test para que soporte monitoreo

--- service-test/src/main/java/org/pasut/demo/microservices/spring/test/Application.java
+++ service-test/src/main/java/org/pasut/demo/microservices/spring/test/Application.java
@@ -1,5 +1,7 @@
 package org.pasut.demo.microservices.spring.test;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration //como agregue la dependencia web (tomcat + spring MVC), asume que estoy creando una aplicacion web
 @RefreshScope
 @EnableEurekaClient
+@Configuration
+@EnableHystrix
 public class Application {
     private final static Logger log = LoggerFactory.getLogger(Application.class);
 
     @RequestMapping("/")
+    @HystrixCommand(threadPoolProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     String hello(@Value("${greeting}") String hello) {
         log.info("Call to hello: " + hello);
         return hello;
