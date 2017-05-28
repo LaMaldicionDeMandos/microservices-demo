7ffb1947d6c51474a99edc678c151c2e8e43a8b6
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-05-28 13:54:40 -0300

    agrego actuator para usar remote config

--- monitor-service/src/main/java/org/pasut/demo/microservices/monitor/Application.java
+++ monitor-service/src/main/java/org/pasut/demo/microservices/monitor/Application.java
@@ -2,12 +2,16 @@ package org.pasut.demo.microservices.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableTurbine
+@EnableEurekaClient
+@Configuration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
